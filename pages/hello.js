7c1efb44cdd3d54188d71b97730dc79e21b738a0
sulemansarwar10 <sulemansarwar10@gmail.com>
2022-08-20 02:14:25 +0500

    setup nextjs app

---
+++ pages/hello.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Hello = () => {
+  return (
+    <div>Hello
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+
+    </div>
+  )
+}
+
+export default Hello
