c3b2bec09f28da988907257a73e1a860a3e38439
sulemansarwar10 <sulemansarwar10@gmail.com>
2022-10-21 22:14:52 +0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */ 
 module.exports = {
-  purge: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+  content: [
+    "./pages/**/*.js",
+    "./components/**/*.js",
   ],
   theme: {
     extend: {},
