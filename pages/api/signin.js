1b71bf5ff3aa874ebe35133c7aafc21aaa9b8055
sulemansarwar10 <sulemansarwar10@gmail.com>
2022-10-23 15:50:37 +0500

    Update signin.js

--- pages/api/signin.js
+++ pages/api/signin.js
@@ -9,15 +9,23 @@ const handler = async (req, res) => {
     if (req.method === 'POST') {
 
         try {
+            let data
+            try {
+                data = JSON.parse(req.body)
 
-            let user = await User.findOne({ email: req.body.email })
+            } catch (error) {
+                data = req.body
+
+            }
+            const { email, password } = data
+            let user = await User.findOne({ email: email })
             if (!user) {
                 return res.status(400).json({ success: false, msg: "sorry! this email is not registered" })
             }
             //adding salt to the password to secure and generating hash of password
             const secpass = await CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY).toString(CryptoJS.enc.Utf8);;
 
-            if (secpass === req.body.password) {
+            if (secpass === password) {
                 const data = {
                     user: {
                         name: user.fname,
